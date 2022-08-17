94bda0874347b40554af6670c268d470c7a0644c
Dominic Griesel <dominic.griesel@nabucasa.com>
2022-08-17 17:00:28 +0200

    chore: some fixes in make_hybrid script

--- .github/make_hybrid.sh
+++ .github/make_hybrid.sh
@@ -7,21 +7,25 @@ mv test.js test.mjs
 
 PJSON=$(cat package.json | jq --tab '
 	del(.type)
+	| .description = .description + ". This is a fork of " + .repository + ", but with CommonJS support."
+	| .repository = "esm2cjs/" + .name
+	| .name |= "@esm2cjs/" + .
+	| .author = { "name": "Dominic Griesel", "email": "d.griesel@gmx.net" }
+	| .publishConfig = { "access": "public" }
+	| .funding = "https://github.com/sponsors/AlCalzone"
 	| .main = "cjs/index.js"
 	| .module = "esm/index.js"
 	| .exports = {}
 	| .exports["."].import = "./esm/index.js"
 	| .exports["."].require = "./cjs/index.js"
 	| .exports["./package.json"] = "./package.json"
-	| .xo = {ignores: ["cjs"]}
-	| .description = .description + ". This is a fork of " + .repository + ", but with CommonJS support."
-	| .repository = "esm2cjs/" + .name
-	| .funding = "https://github.com/sponsors/AlCalzone"
-	| .name |= "@esm2cjs/" + .
 	| .scripts["to-cjs"] = "esm2cjs --in esm --out cjs -t node12"
-	'
-)
+	| .xo = {ignores: ["cjs"]}
+')
 echo "$PJSON" > package.json
 
 npm i -D @alcalzone/esm2cjs
 npm run to-cjs
+
+PJSON=$(cat package.json | jq --tab 'del(.scripts["to-cjs"])')
+echo "$PJSON" > package.json
